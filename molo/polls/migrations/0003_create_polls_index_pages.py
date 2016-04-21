5696a8fa7339c8ca7aa6a93da3ddfbb2fca91a75
Saeed Marzban <Saeed.mb8@gmail.com>
2016-04-21 14:34:50 +0200

    removed moving pages from the migration

--- molo/polls/migrations/0003_create_polls_index_pages.py
+++ molo/polls/migrations/0003_create_polls_index_pages.py
@@ -6,21 +6,13 @@ from django.db import migrations, models
 
 def create_polls_index(apps, schema_editor):
     from molo.core.models import Main
-    from molo.polls.models import Question, FreeTextQuestion, PollsIndexPage
+    from molo.polls.models import PollsIndexPage
     main = Main.objects.all().first()
 
     if main:
         polls_index = PollsIndexPage(title='Polls', slug='polls')
         main.add_child(instance=polls_index)
         polls_index.save_revision().publish()
-
-        # Move existing Questions
-        for page in Question.objects.all().child_of(main):
-            page.move(polls_index, pos='last-child')
-
-        # Move existing FreeTextQuestion
-        for page in FreeTextQuestion.objects.all().child_of(main):
-            page.move(polls_index, pos='last-child')
 
 
 class Migration(migrations.Migration):
