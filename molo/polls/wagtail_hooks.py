d086cec9d0d38e721ed1d0280dfac48fc9bb22f8
Malte Meister <malte@labs.ws>
2016-07-28 14:46:53 +0200

    TUNE-142: move QuestionsModelAdmin to admin.py

--- molo/polls/wagtail_hooks.py
+++ molo/polls/wagtail_hooks.py
@@ -1,10 +1,8 @@
 from django.conf.urls import url
-from django.core.urlresolvers import reverse
-from molo.polls.admin import QuestionAdmin
+from molo.polls.admin import QuestionsModelAdmin
 from molo.polls.admin_views import QuestionResultsAdminView
-from molo.polls.models import Question
 from wagtail.wagtailcore import hooks
-from wagtailmodeladmin.options import ModelAdmin, wagtailmodeladmin_register
+from wagtailmodeladmin.options import wagtailmodeladmin_register
 
 
 @hooks.register('register_admin_urls')
@@ -14,21 +12,5 @@ def register_question_results_admin_view_url():
             QuestionResultsAdminView.as_view(),
             name='question-results-admin'),
     ]
-
-
-class QuestionsModelAdmin(ModelAdmin, QuestionAdmin):
-    model = Question
-    menu_label = 'Polls'
-    menu_icon = 'doc-full'
-    add_to_settings_menu = False
-    list_display = ('entries', 'live')
-
-    def entries(self, obj, *args, **kwargs):
-        url = reverse('question-results-admin', args=(obj.id,))
-        return '<a href="%s">%s</a>' % (url, obj)
-
-    entries.allow_tags = True
-    entries.short_description = 'Title'
-
 
 wagtailmodeladmin_register(QuestionsModelAdmin)
