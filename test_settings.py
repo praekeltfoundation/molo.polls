d7f7887f8567d18104d894c7a0ab955df09565f0
nathanbegbie <nathanbegbie@gmail.com>
2017-04-11 07:53:43 +0200

    Change settings set up to use template overrides in test settings

--- test_settings.py
+++ test_settings.py
@@ -1,4 +1,7 @@
 from testapp.settings import *  # noqa
+from os.path import abspath, dirname, join
+
+PROJECT_ROOT = dirname(dirname(dirname(abspath(__file__))))
 
 DATABASES = {
     'default': {
@@ -12,46 +15,27 @@ CELERY_ALWAYS_EAGER = True
 
 DEFAULT_SITE_PORT = 8000
 
-INSTALLED_APPS = [
-    'django.contrib.auth',
-    'django.contrib.admin',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'django.contrib.sites',
-    'django_extensions',
-
-    'taggit',
-    'modelcluster',
-
-    'testapp',
-    'molo.core',
-    'molo.profiles',
-    'mote',
-    'google_analytics',
-
-    'wagtail.wagtailcore',
-    'wagtail.wagtailadmin',
-    'wagtail.wagtaildocs',
-    'wagtail.wagtailsnippets',
-    'wagtail.wagtailusers',
-    'wagtail.wagtailsites',
-    'wagtail.wagtailimages',
-    'wagtail.wagtailembeds',
-    'wagtail.wagtailsearch',
-    'wagtail.wagtailredirects',
-    'wagtail.wagtailforms',
-    'wagtailmedia',
-    'wagtail.contrib.wagtailsitemaps',
-    'wagtail.contrib.settings',
-    'wagtail.contrib.modeladmin',
-
-    'mptt',
-    'djcelery',
-    'molo.polls',
-
-    'raven.contrib.django.raven_compat',
-    'django_cas_ng',
-    'compressor',
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            join(PROJECT_ROOT, 'testapp', 'templates'),
+        ],
+        'APP_DIRS': False,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+                'molo.core.context_processors.locale',
+                'wagtail.contrib.settings.context_processors.settings',
+            ],
+            "loaders": [
+                "django.template.loaders.filesystem.Loader",
+                "mote.loaders.app_directories.Loader",
+                "django.template.loaders.app_directories.Loader",
+            ]
+        },
+    },
 ]
