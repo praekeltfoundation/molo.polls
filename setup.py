f2d2c8019873b3b762dcb47ec90f1678742309d9
Codie Roelf <codie@praekelt.com>
2018-01-15 16:36:11 +0000

    add python3 to travis

--- setup.py
+++ setup.py
@@ -3,24 +3,25 @@ import os
 from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(here, 'README.rst')) as f:
-    readme = f.read()
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
 
-with open(os.path.join(here, 'requirements.txt')) as f:
-    requires = filter(None, f.readlines())
+with open('requirements.txt') as req_file:
+    requires = req_file.read().split('\n')
 
-with open(os.path.join(here, 'requirements-dev.txt')) as f:
-    requires_dev = filter(None, f.readlines())
+with open('requirements-dev.txt') as req_file:
+    requires_dev = req_file.read().split('\n')
 
-with open(os.path.join(here, 'VERSION')) as f:
-    version = f.read().strip()
+with open('VERSION') as fp:
+    version = fp.read().strip()
 
 setup(name='molo.polls',
       version=version,
       description=('A molo module that provides the ability to run polls.'),
       long_description=readme,
       classifiers=[
-          "Programming Language :: Python",
+          "Programming Language :: Python 2.6",
+          "Programming Language :: Python 3.6",
           "Framework :: Django",
           "Topic :: Internet :: WWW/HTTP",
           "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
